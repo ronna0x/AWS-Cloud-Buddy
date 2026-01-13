b89f99f95a6615bd952813c68282c541debd0569
Liza George <lizasusan93@gmail.com>
2026-01-12 21:52:35 -0500

    Local URL changed to cloudfront uRL

--- frontend (1)/frontend/src/api/auth.js
+++ frontend (1)/frontend/src/api/auth.js
@@ -6,7 +6,7 @@ export const LOGIN_URL =
   "?client_id=6v9ml7acqu4oksdbvo188t20nl" +
   "&response_type=code" +
   "&scope=openid+email+phone" +
-  "&redirect_uri=http://localhost:5173";
+  "&redirect_uri=https://d2jt6kdflh9if4.cloudfront.net";
 
 export const getCodeFromUrl = () => {
   const params = new URLSearchParams(window.location.search);
@@ -18,7 +18,7 @@ export const exchangeCodeForToken = async (code) => {
     grant_type: "authorization_code",
     client_id: "6v9ml7acqu4oksdbvo188t20nl",
     code,
-    redirect_uri: "http://localhost:5173",
+    redirect_uri: "https://d2jt6kdflh9if4.cloudfront.net",
   });
 
   const res = await fetch(
